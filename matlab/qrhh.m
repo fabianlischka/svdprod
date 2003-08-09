c1693e50ccdbf4dae6def604dd8744b3d00332ff
Fabian R Lischka <fabian.lischka@me.com>
2003-08-10 07:58:15 +1200

    collect beta and v, construct Q

--- matlab/qrhh.m
+++ matlab/qrhh.m
@@ -5,8 +5,9 @@ M = size( A, 1 );   % rows
 N = size( A, 2 );   % cols
 K = min( M, N );    % min = steps
 
-Q = zeros(M, K );   % Q will be M x K
+Q = eye(M, K);      % Q will be M x K
 R = A(1:K,:);       % R will be K x N
+betas = zeros( K );
 
 for k = 1:(K-1)
     % determine HH reflector for column A(k:K,k)
@@ -15,7 +16,8 @@ for k = 1:(K-1)
     x1 = v(1);
     sigma = v(2:end)'*v(2:end);
     if sigma == 0
-        R(k,k) = abs( x1 );  % = norm(x). No need to update rest of matrix, since beta = 0
+        R(k,k) = abs( x1 );  % = norm(x). No need to update rest of matrix (or betas), since beta = 0
+        R((k+1):K,k) = 0;
 	else    % note: here, we always choose v = x - norm(x) * e1, ie v(1) always < 0
         mu = sqrt( x1^2 + sigma );  % = norm( x )
         if x1 <= 0
@@ -25,12 +27,22 @@ for k = 1:(K-1)
             % but because of potential cancellation written as  
             % (x(1)^2 - mu^2)/(x(1)+mu)
         end;
-        beta = 2*v(1)^2/( sigma + v(1)^2 );
+        betas( k ) = 2*v(1)^2/( sigma + v(1)^2 );
         v = v./ v(1);
         % HH reflector now: I - beta * v * v'. Note: v(1) = 1
         
         R(k,k) = mu;                            % = norm(x)
-        R(k:K,(k+1):K) = R(k:K,(k+1):K) - beta * v * v' * R(k:K,(k+1):K);
-        R((k+1):K,k) = 0;
+        R(k:K,(k+1):K) = R(k:K,(k+1):K) - betas( k ) * v * v' * R(k:K,(k+1):K);
+        % collect Qi
+        R((k+1):K,k) = v(2:end);
 	end;
+end;
+
+% compute Q, and zero out R
+for k = (K-1):-1:1
+    % HH reflector now: I - beta * v * v'. Note: v(1) = 1
+    v(k)=1;
+    v((k+1):K) = R((k+1):K,k);
+    Q(k:K,k:K) = Q(k:K,k:K) - betas( k ) * v(k:K) * transpose(v(k:K)) * Q(k:K,k:K);
+    R((k+1):K,k) = 0;
 end;
