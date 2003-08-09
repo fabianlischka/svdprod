c2e35549a69f8ed1b571d35ac3c62e83aa30bb2c
Fabian R Lischka <fabian.lischka@me.com>
2003-08-10 05:53:59 +1200

    no need for x, just x1. No need to update R for beta = 0.

--- matlab/qrhh.m
+++ matlab/qrhh.m
@@ -11,26 +11,26 @@ R = A(1:K,:);       % R will be K x N
 for k = 1:(K-1)
     % determine HH reflector for column A(k:K,k)
     % [ v, beta ] = house( A(k:K,k) );
-    x = R(k:K,k);
-    sigma = x(2:end)'*x(2:end);
-    v     = x; v(1) = 1;  % the second assignment is for the case sigma == 0
+    v  = R(k:K,k);
+    x1 = v(1);
+    sigma = v(2:end)'*v(2:end);
     if sigma == 0
-        beta = 0; mu = x(1);
+        R(k,k) = abs( x1 );  % = norm(x). No need to update rest of matrix, since beta = 0
 	else    % note: here, we always choose v = x - norm(x) * e1, ie v(1) always < 0
-        mu = sqrt( x(1)^2 + sigma );  % = norm( x )
-        if x(1) <= 0
-            v(1) = x(1) - mu;
+        mu = sqrt( x1^2 + sigma );  % = norm( x )
+        if x1 <= 0
+            v(1) = x1 - mu;
         else
-            v(1) = -sigma/( x(1) + mu );  % this is also x(1) - mu,
+            v(1) = -sigma/( x1 + mu );  % this is also x(1) - mu,
             % but because of potential cancellation written as  
             % (x(1)^2 - mu^2)/(x(1)+mu)
         end;
         beta = 2*v(1)^2/( sigma + v(1)^2 );
         v = v./ v(1);
+        % HH reflector now: I - beta * v * v'. Note: v(1) = 1
+        
+        R(k,k) = mu;                            % = norm(x)
+        R(k:K,(k+1):K) = R(k:K,(k+1):K) - beta * v * v' * R(k:K,(k+1):K);
+        R((k+1):K,k) = 0;
 	end;
-    % HH reflector now: I - beta * v * v'. Note: v(1) = 1
-    
-    R(k,k) = mu;  % = norm(x)
-    R((k+1):K,k) = 0;
-    R(k:K,(k+1):K) = R(k:K,(k+1):K) - beta * v * v' * R(k:K,(k+1):K)
 end;
