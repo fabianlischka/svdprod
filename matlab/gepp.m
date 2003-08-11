dfb43a3cbdee063b86942ff41088d5cac5540289
Fabian R Lischka <fabian.lischka@me.com>
2003-08-12 01:05:34 +1200

    also return mu = norm of x, add header

--- matlab/gepp.m
+++ matlab/gepp.m
@@ -6,28 +6,43 @@ N = size( A, 2 )    % cols
 K = min( M, N )     % min = steps
 
 U = A;              % U will be K x N
-L = eye( M, K );    % L will be M x K
+L = zeros( M, K );  % L will be M x K
+P = 1:K;            % this contains the permutation: row k has been permuted with P(k)
 
 % iterative version: k=steps m=rows n=cols
-if 0
+if 1
 for k = 1:(K-1)
+    P
+    L
+    U
     % choose pivot
-    [ Y, Index ] = max(abs(U(k,:)));
-    % pivot is U(k,Index)
-    % iterate through rows below
-    for m = (k+1):M
-        mult = U(m, Index) / U(k, Index);
+    [ Y, IndirectIndex ] = max(abs(U(P(k:K),k)));
+    Index = P( k-1+IndirectIndex )
+    % pivot is U(Index,k)
+    temp        = P(Index);
+    P(Index)    = P(k);
+    P(k)        = temp;
+    % note: now P(1:(k-1)) contains rows that have been done, P(k) == Index
+    % contains the row we do now, P((k+1):K) contains untouched rows (which
+    % need to be touched)
+    L( Index, k ) = 1;
+    % iterate through other rows: these are in P((k+1):K)
+    for m = P((k+1):K)
+        mult = U(m, k) / U( Index, k )   % note: Y == abs( U( Index, k ) ), ie Y is abs(pivot), not the pivot value itself...
         L(m, k) = mult;
-        for n = 1:N
-            U(m, n) = U(m, n) - mult * U(k, n);
+        for n = 1:N     % need not be 1:N - need to think
+            U(m, n) = U(m, n) - mult * U(Index, n);
         end
     end
+    P
+    L
+    U
 end
 end
 
 
 % vectorized version: k m n
-if 1
+if 0
 for k = 1:(K-1)
     % choose pivot
     [ Y, Index ] = max(abs(U(k,:)));
---
+++ matlab/house.m
@@ -0,0 +1,28 @@
+function [ v, beta, mu ] = house( x )
+% HOUSE computes v with v(1) = 1 and beta such that F = I - beta v v' is
+% orthogonal, and Fx = sqrt( x'x ) e_1
+% in other words, F is the reflector that reflects x onto  
+% (the positive part of) the first coordinate axis
+% v = x - norm(x) * e1
+%
+% $Header$
+
+sigma = x(2:end)'*x(2:end);
+v     = x; v(1) = 1;  % the second assignment is for the case sigma == 0
+if sigma == 0
+    beta = 0;
+    mu = abs( x(1) );   % == norm( x );
+else    % note: here, we always choose v = x - norm(x) * e1, ie v(1) always < 0, and Fx(1)>0
+    mu = sqrt( x(1)^2 + sigma );  % == norm( x )
+    if x(1) <= 0
+        v(1) = x(1) - mu;
+    else
+        v(1) = -sigma/( x(1) + mu );  % this is also x(1) - mu,
+        % but because of potential cancellation written as  
+        % (x(1)^2 - mu^2)/(x(1)+mu)
+    end;
+    beta = 2*v(1)^2/( sigma + v(1)^2 );
+    v = v./ v(1);
+end;
+
+% HH reflector now: I - beta * q * q'
