9804901700f63b54d63796e97e86a43a75027711
Fabian R Lischka <fabian.lischka@me.com>
2003-08-18 10:51:22 +1200

    added Hilbert, Dorr, Kahan matrices, and negative random

--- matlab/gentestmat.m
+++ matlab/gentestmat.m
@@ -6,18 +6,24 @@ As      = { '-triu( ones( M ) ) + 2 * diag( ones( M, 1 ) )',
                 'uppertriang, structured, eval = 1, only 1 evect, bad condition';
             'triu( 10*rand( M ) - 1 ) - 20 * diag( ones( M, 1 ) )',
                 'uppertriang, large negative diag';
-            'rand( M, N )',
-                'elements uniformly random 0..1';
+            '2*rand( M, N ) - 1',
+                'elements uniformly random -1..1';
             'exp(16*rand( M, N )-15).*(rand( M, N )-.5)',  
                 'elements of very different size';
             'U* diag(2.^(-1:-1:-N)) *V',         
                 'very different singular values: 1/2 .. 1/(2^N)';
-            'repmat( randn( M, 1 ), 1, N ) + 1e-15*rand( M, N )', 
-                'rank 1 matrix with slight disturbances';
-            'W* ( diag(1e-6*ones(M,1)) + diag(ones(M-1,1),1) ) * transpose(W)'
+            '10 * randn( M, 1 ) * rand( 1, N ) + 1e-15*rand( M, N )', 
+                'rank 1 matrix with slight perturbations';
+            'W* ( diag(1e-6*ones(M,1)) + diag(ones(M-1,1),1) ) * transpose(W)',
                 '1 small eval, one evect';
-            'W* ( diag(1e+6*ones(M,1)) + diag(ones(M-1,1),1) ) * transpose(W)'
+            'W* ( diag(1e+6*ones(M,1)) + diag(ones(M-1,1),1) ) * transpose(W)',
                 '1 big   eval, one evect';
+            'hilb( M )',
+                'Hilbert matrix';
+            'full( gallery( ''dorr'', M, 0.02 ) )',
+                'Dorr matrix';
+            'gallery(''kahan'', [ M, N ] )'
+                'Kahan matrix';
           };
 
 if nargin == 2 || N > M
