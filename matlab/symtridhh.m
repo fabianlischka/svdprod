36e80d5a1cc2bf7353aa290fb5af596b55e39cea
Fabian R Lischka <fabian.lischka@me.com>
2003-08-18 05:51:47 +1200

    use norm mu returned from house, no need to recompute

--- matlab/symtridhh.m
+++ matlab/symtridhh.m
@@ -7,10 +7,10 @@ if any(any(A ~= A'))
 end
 n = size( A, 1 );
 for k = 1:(n-2)
-    [ v, beta ] = house( A( (k+1):n, k ) );
+    [ v, beta, mu ] = house( A( (k+1):n, k ) );
     p = beta*A( (k+1):n, (k+1):n )*v;
     w = p - (beta*p'*v/2)*v;
-    A(k+1,k) = norm( A( (k+1):n,k ) );  A( (k+2):n, k ) = 0;
+    A(k+1,k) = mu                    ;  A( (k+2):n, k ) = 0;
     A(k,k+1) = A(k+1,k)              ;  A( k, (k+2):n ) = 0;
     % FIX: take advantage of symmetry here
     A( (k+1):n, (k+1):n ) = A( (k+1):n, (k+1):n ) - v*w' - w*v';
