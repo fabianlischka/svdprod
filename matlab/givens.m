d9b385ae15c65a0ebb4de8075706e6b1c7035096
Fabian R Lischka <fabian.lischka@me.com>
2003-08-20 06:56:44 +1200

    initial rev

---
+++ matlab/givens.m
@@ -0,0 +1,17 @@
+function [ c, s ] = givens( a, b )
+% GIVENS computes c and s such that [ c -s; s c ] [ a b ]' = [ r 0 ]'
+% $Id$
+
+if b == 0
+    c = 1; s = 0;
+else
+    if abs( b ) > abs( a )
+        tau = -a/b; s = 1/sqrt( 1+tau^2 ); c = s*tau;
+    else
+        tau = -b/a; c = 1/sqrt( 1+tau^2 ); s = c*tau;
+    end;
+end;
+
+% flop count: 5 and 1 sqrt
+% protected agains overflow
+% reference: Golub, Van Loan; 3rd ed; 5.1.8
