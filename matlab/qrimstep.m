79603dc0ff0f3f2ed16f78d1dd681d0e195e5d9e
Fabian R Lischka <fabian.lischka@me.com>
2003-08-25 10:15:01 +1200

    functionised it, switched to short TS format, update Q

--- matlab/qrimstep.m
+++ matlab/qrimstep.m
@@ -1,3 +1,4 @@
+function [ TS, Q ] = qrimstep( TS, Q ) 
 % symmetric implicit QR step with Wilkinson shift
 % given a symmetric tridiagonal T, routine computes T+ = Q'TQ, 
 % where QR = T - mu I  is a QR decomposition of T shifted by mu
@@ -9,9 +10,7 @@
 % reference: Golub, Van Loan; 3rd ed; ch. 8.3.5
 % $Id$
 
-    	
-    TS = stridl2s( TL );
-    N = size( TL, 1 );
+    N = size( TS, 1 );
     % note: T in short format
     
     d       = ( TS( N-1, 1 ) - TS( N, 1 ) ) / 2;    % flops: 2
@@ -30,16 +29,18 @@
     
     % note: later, we would want to pass a matrix in to be updated
     % (instead of starting from the identity, and then multiply...)
-    Q       = eye( N );
-    
+    % Q       = eye( N );
+    % done...
+
     for k = 1:(N-1)
         % compute Givens rotation, around k,k+1, G = [c s; -s c]
         [ c s ] = givens( x, y );                           % flops: 5 + 1 sqrt
         % rotate T: want T+ = G' T G, and Q+ = Q G
         G               = [ c s; -s c ];
         K               = min( N, k+2 );                    % flops: 1, or so...
+        % update Q
         Q( 1:K, k:k+1 ) = Q( 1:K, k:k+1 ) * G;              % flops: 6 * K
-        % for long T, we want:
+        % for T in ordinary format, we want:
         % T( k:k+1, k-1:K ) = G' * T( k:k+1, k-1:K );
         % T( k-1:K, k:k+1 ) = T( k-1:K, k:k+1 ) * G;
         
@@ -60,5 +61,5 @@
         y            = bulge;       % which we want to eliminate...
     end;
     TS( N, 2 ) = 0;
-
+	
     % flop count: around 40 N for new TS, around 3 N^2 for accumulating Q
