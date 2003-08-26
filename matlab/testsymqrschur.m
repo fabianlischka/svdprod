560da48fd60b03b2e33b5b101f1f85cea97ad76e
Fabian R Lischka <fabian.lischka@me.com>
2003-08-27 11:00:14 +1200

    also test A-Q'DQ, and orthogonality Q'Q-I

--- matlab/testsymqrschur.m
+++ matlab/testsymqrschur.m
@@ -4,15 +4,18 @@ N = 60;
 
 disp( 'We compute eigenvalues of a test matrix using eig(), and then using the schur decomposition' );
 disp( 'computed by symqrschur, and display the norm of the difference of the vector of eigenvalues' );
-disp( 'and also relative to the norm of the vector of eigenvalues itself. The latter should always' );
-disp( 'be around 1e-15.' );
+disp( 'relative to the norm of the vector of eigenvalues itself. Also the norm of A-QDQ'', and Q''Q-I.' );
+disp( 'All errors should be around 1e-15.' );
 disp( sprintf( '\nTesting SYMQRSCHUR with various (symmetrized) %g x %g matrices', N, N )); 
 for Typ = 1:14
 	TestMat = gentestmat( Typ, N, N, 0 );
     TestMat = TestMat + TestMat';
     Ev      = eig( TestMat );
-    ErrEv   = sort(symqrschur( TestMat ) ) - sort( Ev );
-    disp( sprintf( 'for type %g', Typ ) );
-    disp( sprintf( 'err norms: 1norm: %12g,  2norm: %12g,  infnorm: %12g', norm( ErrEv, 1 ), norm( ErrEv, 2 ), norm( ErrEv, 'inf' ) ) );
-    disp( sprintf( 'rel norms: 1norm: %12g,  2norm: %12g,  infnorm: %12g', norm(ErrEv,1)/norm(Ev,1), norm(ErrEv,2)/norm(Ev,2), norm(ErrEv,'inf')/norm(Ev,'inf') ) );
+    [ D,Q ] = symqrschur( TestMat );
+    ErrEv   = sort( D ) - sort( Ev );
+    Res     = norm( TestMat - Q*diag(D)*Q' );
+    RelRes  = Res / norm( TestMat );
+    Orth    = norm( Q'*Q - eye( N ) );
+    % disp( sprintf( 'eval 1norm: %12g, infnorm: %12g; Res: %12g', norm( ErrEv, 1 ), norm( ErrEv, 'inf' ), Res ) );
+    disp( sprintf( 'Type %2g: eval 1norm: %12g, infnorm: %12g; Res: %12g, Orth: %12g', Typ, norm(ErrEv,1)/norm(Ev,1), norm(ErrEv,'inf')/norm(Ev,'inf'), RelRes, Orth ) );
 end;
