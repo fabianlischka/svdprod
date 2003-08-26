ac86884da6197bfe683a06024c98ece92e453136
Fabian R Lischka <fabian.lischka@me.com>
2003-08-27 03:33:02 +1200

    functionized it, compute U,V if desired

--- matlab/bidighh.m
+++ matlab/bidighh.m
@@ -1,7 +1,9 @@
-% bidiagonalization using householder
-% suppose A MxN, M >= N, A real. We will find orthogonal U MxM, V NxN 
-% such that R = U'AV is bidiagonal (the diagonal and one superdiagonal)
-% note: U, V not actually computed here
+function [ B, Q, U ] = bidighh( A );
+% BIDIGHH computes bidiagonal B=U'AV using householder reflections
+% suppose A MxN, M >= N, A real. This routine finds orthogonal U MxM, V NxN 
+% such that B = U'AV is bidiagonal (the diagonal and one superdiagonal).
+% The diagonal is stored in the first col of B, the superdiagonal in the
+% second col of B, and B(end,2) == 0
 
 % reference: Golub, Van Loan; 3rd ed; 5.4.3
 % $Id$
@@ -12,30 +14,30 @@ if M < N
     err( 'A must have M >= N' );
 end;
 
-R       = A;              % R will be M x N
+B       = A;              % B will be M x N
 betas   = zeros( N-1, 1 );
 gammas  = zeros( N-2, 1 );
 
-for k = 1:N
+for k = 1:N-1
     % determine HH reflector for column A(k:N,k)
-    [ v, beta, mu ] = house( R(k:M,k) );            % flops: 3(M-k) 
+    [ v, beta, mu ] = house( B(k:M,k) );            % flops: 3(M-k) 
     betas( k )      = beta;
-    R(k,k)          = mu;                           % = norm(x)
-    R(k:M,(k+1):N)  = R(k:M,(k+1):N) - beta * v * v' * R(k:M,(k+1):N);
+    B(k,k)          = mu;                           % = norm(x)
+    B(k:M,(k+1):N)  = B(k:M,(k+1):N) - beta * v * v' * B(k:M,(k+1):N);
                                                     % flops: 4*(N-k)*(M-k)
-    % R((k+1):M,k)    = v(2:end);
-    % or, if you don't want to collect it, R((k+1):M,k) = 0;
-    R((k+1):M,k)    = 0;
+    B((k+1):M,k)    = v(2:end);
+    % or, if you don't want to collect it, B((k+1):M,k) = 0;
+    % B((k+1):M,k)    = 0;
     if k < N - 1
-		[ v, beta, mu ] = house( R(k,(k+1):N)' );   % flops: 3(N-k)
+		[ v, beta, mu ] = house( B(k,(k+1):N)' );   % flops: 3(N-k)
 		gammas( k )     = beta;
-		R(k,k+1)        = mu;                        % = norm(x)
+		B(k,k+1)        = mu;                        % = norm(x)
         % note that we need to multiply from the right here:
-		R((k+1):M,(k+1):N)  = R((k+1):M,(k+1):N) - beta * R((k+1):M,(k+1):N) * v * v';
+		B((k+1):M,(k+1):N)  = B((k+1):M,(k+1):N) - beta * B((k+1):M,(k+1):N) * v * v';
                                                     % flops: 4*(N-k)*(M-k)
-		% R(k,(k+2):N)    = v(2:end)';
-        % or, if you don't want to collect it, R(k,(k+2):N) = 0;
-        R(k,(k+2):N)    = 0;
+		B(k,(k+2):N)    = v(2:end)';
+        % or, if you don't want to collect it, B(k,(k+2):N) = 0;
+        % B(k,(k+2):N)    = 0;
     end
 end;
 
@@ -44,11 +46,24 @@ end;
 % = 4 MN^2 - 4/3 N^3 = 4 N^2 (M-N/3), or, 
 % for M=N, 8/3 N^3
 
-% if desired, compute Q, and zero out R
-%for k = (N-1):-1:1
-    % HH reflector now: I - beta * v * v'. Note: v(1) = 1
-%    v(k)=1;
-%    v((k+1):N) = R((k+1):N,k);
-%    Q(k:N,k:N) = Q(k:N,k:N) - betas( k ) * v(k:N) * transpose(v(k:N)) * Q(k:N,k:N);
-%    R((k+1):N,k) = 0;
-%end;
+if nargout > 1
+    % compute U and V
+    v = zeros( M, 1 );
+    U = eye( M );
+    for k = (N-1):-1:1
+        % HH reflector now: I - beta * v * v'. Note: v(1) = 1
+        v(k)       = 1;
+        v((k+1):M) = B((k+1):M,k);
+        U(k:M,k:M) = U(k:M,k:M) - betas( k ) * v(k:M) * v(k:M)' * U(k:M,k:M);
+        B((k+1):M,k) = 0;
+    end;
+    V = eye( N );
+    for k = (N-2):-1:1
+        v(k+1)      = 1;
+        v((k+2):N)  = B(k,(k+2):N)';
+        V(k+1:N,k+1:N) = V(k+1:N,k+1:N) - gammas( k ) * v(k+1:N) * v(k+1:N)' * V(k+1:N,k+1:N);
+        B(k,(k+2):N) = 0;
+    end;
+else
+    B = [ diag(B) [ diag(B,1); 0 ] ];
+end;
