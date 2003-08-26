bb0003f130c5768ba1784215f38ea82ba855ae1c
Fabian R Lischka <fabian.lischka@me.com>
2003-08-27 04:06:58 +1200

    also test transpose, print rel SV error

--- matlab/testbidighh.m
+++ matlab/testbidighh.m
@@ -3,16 +3,17 @@
 N = 20;
 for M = 20:10:30
     disp( sprintf( '\nTesting BIDIGHH with various %g x %g matrices', M, N )); 
-    for Typ = 1:13
-        TestMat = gentestmat( Typ, N, N, 0 );
+    for Typ = 1:14
+        TestMat = gentestmat( Typ, M, N, 0 );
         disp( sprintf( 'for type %g', Typ ) );
-        for trans = 0:1
+        for trans = 0:(M==N)
             [B,U,V] = bidighh( TestMat );
             dev     = norm( B - U'*TestMat*V );
             reldev  = dev / norm( TestMat );
             svdB    = sort( svd( B ) );
             svdA    = sort( svd( TestMat ) );
-            disp( sprintf( 'err norm: %12g,  rel norm: %12g,  infnorm SVs: %12g', dev, reldev, norm( svdA - svdB, 'inf' ) ) );
+            devsvd  = norm( svdA - svdB, 'inf' );
+            disp( sprintf( 'err: %12g,  rel err: %12g,  norm SVs: %12g, rel norm SVs: %12g', dev, reldev, devsvd, devsvd/norm( svdA, 'inf' ) ) );
             TestMat = TestMat';
         end;
     end;
