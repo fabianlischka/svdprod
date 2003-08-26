9bf4ce001a251e867dff1024d4c823cb63169ce2
Fabian R Lischka <fabian.lischka@me.com>
2003-08-26 20:01:27 +1200

    detect if q_i is zero, but don't fix it yet - need to find any q_i orthogonal to previous ones

--- matlab/qrcgs.m
+++ matlab/qrcgs.m
@@ -1,5 +1,6 @@
 function [Q,R] = qrcgs( A )
-% QR-decomposition, Classical Gram Schmidt (numerically unstable)
+% QR-decomposition, Classical Gram Schmidt
+% (numerically unstable, Q might not be very orthogonal)
 
 % reference: Golub, Van Loan; 3rd ed; 5.2.7
 % $Id$
@@ -15,20 +16,20 @@ Q = A;              % Q will be M x N
 R = zeros( N, N );  % R will be N x N
 
 for k = 1:N
-    % orthogonalisieren: iterative version, old
-    % for n = 1:(k-1)
-    %     R(n,k) = Q(:,n)' * A(:,k);              % scalar product: M mult, M-1 add
-    %     Q(:,k) = Q(:,k) - Q(:,n) * R(n,k);      % M mult, M add
-    % end
-    
     % orthogonalisieren: compact version
     R(1:(k-1),k) = Q(:,1:(k-1))' * A(:,k);      % matrix product
     Q(:,k)       = A(:,k) - Q(:,1:(k-1)) * R(1:(k-1),k);
     % ie 4M*(k-1) flops
     
     % normalisieren
-    R(k,k) = norm(Q(:,k),2);                    % M mult, M-1 add, 1 sqrt
-    Q(:,k) = Q(:,k) / R(k,k);                   % M mult
+    R(k,k) = norm(Q(:,k),2);                    % flops: M mult, M-1 add, 1 sqrt
+    % if vector empty, we need to create an orthogonal one
+    if R(k,k) == 0
+        % Q(k,k) = 1;
+        % FIX FIX FIX
+    else
+        Q(:,k) = Q(:,k) / R(k,k);                   % flops: M mult
+    end
     % another 3M flops
 end
 
