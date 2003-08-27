136f4683783bc4ed39f52658d2d216a8b017f5b2
Fabian R Lischka <fabian.lischka@me.com>
2003-08-28 01:49:56 +1200

    implement most of the rotation of B when diag element turns zero, compute D

--- matlab/gksvd.m
+++ matlab/gksvd.m
@@ -39,10 +39,44 @@ while q < N
             k = k+1;
         end;
         % now, k == N-q, or B(k,1) == 0, or both
-        if B( k, 1 ) == 0   % FIX FIX: compare with some eps???
-            err;            % rotate that row away, and look again
-        else    % do some real work
-            [ B, U, V ] = gksvdstep( B, U, V );
+        if abs( B( k, 1 ) ) <= eps * norm( B, 'inf' )
+            % now, B( k, 1 ) == 0, p+1 <= k <= N-q, B(N-q,2) == 0 (note:
+            % even if q == 0, by the format we have chosen)
+            % So, to do: if k<N-q, rotate that row away with Givens rotations from
+            % left, G(k,j), j=k+1:N-q
+            % if k==N-q, then apply Givens from the 
+            % right, G(k,j), j=N-q-1:-1:p+1
+            disp('rotating empty diagonal away')
+            if k < N-q
+                bulge = B(k,2);
+                B(k,2)= 0;
+                for j=k+1:N-q
+                    [ c s ] = givens( B(j,1), bulge );
+                    % need to transpose (since mult from left), and 
+                    % transpose again, since need to eliminate upper
+                    % element
+                    G     = [ c s; -s c ]; 
+                    % rotate B from left: B+ = G B,  G=G(k,j)
+                    % zerod =  c * bulge + s * B(j,1)
+                    B(j,1)= -s * bulge + c * B(j,1);
+                    bulge =  s * B(j,2);
+                    B(j,2)=  c * B(j,2);                    
+                    % rotate U from right (since transposed): U+ = U G'
+                    U(:,[k j]) = U(:,[k j]) * G';       % flops: 6*M
+                end
+            else
+                for j=N-q-1:-1:p+1
+                    err;
+                    [ c s ] = givens( 1,2 );
+                    rotate B from right
+                    rotate V from right
+                end
+            end
+        else    % do some real work - GK SVD step for p+1:N-q
+            [ B(p+1:N-q,:), U(:,p+1:N-q), V(:,p+1:N-q) ] = gksvdstep( B(p+1:N-q,:), U(:,p+1:N-q), V(:,p+1:N-q) );
         end;    
     end;
+    % B(:,2)'
+    % disp( sprintf( '%12g   %12g   %12g', (B(1,2)), log(B(2,2)), log(B(3,2)) ) );
 end;
+D = abs( B( :,1 ) );
