bee93d8d2e0004c343288841c13a76c5799b99cb
Fabian R Lischka <fabian.lischka@me.com>
2003-08-27 21:21:47 +1200

    if x already parallel to e_1 (sigma==0), then choose beta 0 or 2, depending on sign of x_1, so that H x = mu e_1

--- matlab/house.m
+++ matlab/house.m
@@ -9,7 +9,16 @@ function [ v, beta, mu ] = house( x )
 sigma = x(2:end)'*x(2:end);                          % flops: 2N
 v     = x;                                           % mem copy: N
 if isempty( sigma ) | sigma == 0
-    beta = 0;
+    if x(1) >= 0
+        beta = 0;
+    else
+        % Note: we make this choice so that the invariance H*x = mu*e_1
+        % always holds (so in this case, H*x = -x = abs(x(1))*e_1)
+        % otherwise we get sign errors in certain cases in routines that
+        % use house and rely on that behaviour (eg. if we don't multiply
+        % the first column, but just set it [mu 0 0 0 0]')
+        beta = 2;
+    end;
     v(1) = 1;
     mu   = abs( x(1) );   % == norm( x );
 else    % note: here, we always choose v = x - norm(x) * e1
@@ -25,5 +34,5 @@ else    % note: here, we always choose v = x - norm(x) * e1
     v = v./ v(1);                                   % flops: N
 end;
 
-% HH reflector now: I - beta * q * q'
+% HH reflector now: I - beta * v * v'
 % flop count: about 3N (+ 10)
