a91622122dadc5d19f7769e8bcc909003b4015a0
Fabian R Lischka <hackerschool@fabian-lischka.de>
2013-10-16 17:20:02 -0400

    only one run, square matrices only

--- matlab/testbidighh.m
+++ matlab/testbidighh.m
@@ -14,22 +14,19 @@ for M = 30:21:72
     fprintf( fid, 'Type:  B-U''AV Res  Relative Res   SVs Rel Err        U Orth        V Orth\n' );
     for Typ = 1:14
         TestMat = gentestmat( Typ, M, N, 0 );
-        for trans = 0:(M==N)
-            [BS,U,V]= bidighh( TestMat );
-            B       = [ diag( BS(:,1) ) + diag( BS(1:end-1,2), 1 ); zeros( M-N, N ) ];
-            dev     = norm( B - U'*TestMat*V );
-            reldev  = dev / norm( TestMat );
-            svdB    = sort( svd( B ) );
-            svdA    = sort( svd( TestMat ) );
-            devsvd  = norm( svdA - svdB, 'inf' )/norm( svdA, 'inf' );
-            Udev    = norm( U'*U - eye( M ) );
-            Vdev    = norm( V'*V - eye( N ) );
-            fprintf( fid, ' %2g: %12g, %12g, %12g, %12g, %12g\n', Typ, dev, reldev, devsvd, Udev, Vdev );
-            if reldev > tol || Udev > tol || Vdev > tol || devsvd > tol
-                fprintf( fid,  '^^^^^^^^^^^^^^^^^^^^ ^^^^^^^^^^^^^^^^^^^^^^^\n' );
-            end
-            TestMat = TestMat';
-        end;
+        [BS,U,V]= bidighh( TestMat );
+        B       = [ diag( BS(:,1) ) + diag( BS(1:end-1,2), 1 ); zeros( M-N, N ) ];
+        dev     = norm( B - U'*TestMat*V );
+        reldev  = dev / norm( TestMat );
+        svdB    = sort( svd( B ) );
+        svdA    = sort( svd( TestMat ) );
+        devsvd  = norm( svdA - svdB, 'inf' )/norm( svdA, 'inf' );
+        Udev    = norm( U'*U - eye( M ) );
+        Vdev    = norm( V'*V - eye( N ) );
+        fprintf( fid, ' %2g: %12g, %12g, %12g, %12g, %12g\n', Typ, dev, reldev, devsvd, Udev, Vdev );
+        if reldev > tol || Udev > tol || Vdev > tol || devsvd > tol
+            fprintf( fid,  '^^^^^^^^^^^^^^^^^^^^ ^^^^^^^^^^^^^^^^^^^^^^^\n' );
+        end
     end;
 end;
 
